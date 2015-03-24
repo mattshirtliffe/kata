2739b78794ed41f227238c6d2d544efc67e3beea
matt.shirtliffe <matt.shirtliffe@gmail.com>
2015-03-24 14:23:50 +0000

    Created function for unit test

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,7 +1,6 @@
 
 
 def fizz_buzz():
-
     for i in range(0,100):
         fizzy_bizzy = ""
         if i % 3 == 0:
@@ -13,7 +12,21 @@ def fizz_buzz():
             fizzy_bizzy += str(i)
 
         print fizzy_bizzy
+
+
+def fizz_buzzy(i):
+
+    fizzy_bizzy = ""
+    if i % 3 == 0:
+        fizzy_bizzy += "Fizz"
+    if i % 5 == 0:
+        fizzy_bizzy += "Buzz"
+    if fizzy_bizzy == "":
+        fizzy_bizzy += str(i)
+
+    return fizzy_bizzy
     
 
 if __name__ == "__main__":
-    fizz_buzz()
+    #fizz_buzz()
+    print fizz_buzzy(90)
